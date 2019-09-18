e3ad5e147454d02de8c74f5efe67f7620fea2eb5
formando <formando@c2-ILCM-M10.local>
2019-09-18 13:34:05 +0100

    First Commit

---
+++ IUE-Worksheet1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IUE-Worksheet1
+//
+//  Created by formando on 18/09/2019.
+//  Copyright © 2019 formando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var goodJob: UILabel!
+    
+    @IBAction func clickButton(_ sender: UIButton) {
+        
+        self.goodJob.text == "Wow!" ? (self.goodJob.text = "Good Job!") : (self.goodJob.text = "Wow!");
+        
+    }
+}
+
